87e0d5852e1bd324349ffe3778c5a5f1ab9a9cec
John Santosuosso <john.santosuosso1@gmail.com>
2024-06-16 19:03:39 -0400

    add dog service and observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  dogPhotos: any;
 
+  constructor(private apiService: ApiService) { }
+
+  onButtonClick() {
+    this.loadDogPhotos();
+  }
+
+  loadDogPhotos() {
+    this.apiService.getDogPhotos().subscribe(data => {
+      this.dogPhotos = data;
+      console.log(this.dogPhotos);
+    },
+    error => {
+      console.log(error);
+    });
+  }
 }
